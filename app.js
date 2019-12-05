ccfde55970d2c44a1deb1e2a4e56fd24380b113e
eparionad <eparionad@gmail.com>
2019-12-04 19:00:52 -0500

    Se maqueta el juego

---
+++ app.js
@@ -0,0 +1,10 @@
+//Piano con Jquery
+/*$(document).ready(function () {
+    $('#padre').on('click', 'div', function(){
+        var atributo = "#"+"a"+$(this).attr('id');
+
+        $(atributo)[0].pause();
+        $(atributo)[0].currentTime=0;
+        $(atributo)[0].play();
+    });
+});*/
