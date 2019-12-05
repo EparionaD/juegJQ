69537bff4251e716eb77f58936a629c0fcb90f99
eparionad <eparionad@gmail.com>
2019-12-04 21:38:59 -0500

    se agrego la funcionalidad para el juego

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-//Piano con Jquery
-/*$(document).ready(function () {
-    $('#padre').on('click', 'div', function(){
-        var atributo = "#"+"a"+$(this).attr('id');
+$(document).ready(function(){
 
-        $(atributo)[0].pause();
-        $(atributo)[0].currentTime=0;
-        $(atributo)[0].play();
+    $('button').on('click', function(e){
+        var nombre = e.currentTarget.dataset.key;
+        var compu = eleccion();
+        $('#cambiar').attr('src',`./img/${nombre}.png`);
+        $('#cambiar1').attr('src',`./img/${compu}.png`);
+        //Eleccion compu
+        function eleccion(){
+            var array = ['piedra', 'papel', 'tijera'];
+            var numero = Math.floor(Math.random()*3);
+            return array[numero];
+        }
+        //Comparacion
+        var resultado = comparacion(nombre,compu);
+        $('#mensaje').html(`Tú ${resultado} escogiendo ${nombre} contra de ${compu}`);
+        console.log(resultado);
+
+        function comparacion(nombre, compu){
+            if(nombre == compu){
+                return "Empate";
+            }
+            if((nombre == 'papel' && compu == 'piedra') || (nombre == 'tijera' && compu == 'papel') || (nombre == 'piedra' && compu == 'tijera')){
+                return "ganas";
+            }else if((nombre == 'papel' && compu == 'tijera') || (nombre == 'tijera' && compu == 'piedra') || (nombre == 'piedra' && compu == 'papel')){
+                return "pierdes";
+            }
+
+        }
     });
-});*/
+});
