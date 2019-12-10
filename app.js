62de658c835b0f596957498734323e62fec9fc47
eparionad <eparionad@gmail.com>
2019-12-09 19:54:12 -0500

    se agrego codigo en javascript

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+//Juego con Jquery
+/*$(document).ready(function(){
 
     $('button').on('click', function(e){
         var nombre = e.currentTarget.dataset.key;
@@ -25,7 +26,44 @@ $(document).ready(function(){
             }else if((nombre == 'papel' && compu == 'tijera') || (nombre == 'tijera' && compu == 'piedra') || (nombre == 'piedra' && compu == 'papel')){
                 return "pierdes";
             }
+        }
+    });
+});*/
 
+
+//Juego con JavaScript
+var botones = document.querySelectorAll(`button`);
+//console.log(botones);
+botones.forEach(function(numero){
+    var boton = numero;
+    //console.log(boton);
+    boton.addEventListener('click', function(e){
+        var nombre = e.currentTarget.dataset.key;
+        //console.log(nombre);
+        var compu = eleccion();
+        //console.log(compu);
+        var cambiar = document.getElementById('cambiar').src=`./img/${nombre}.png`;
+        var cambiar1 = document.getElementById('cambiar1').src=`./img/${compu}.png`;
+        //Eleccion compu
+        function eleccion(){
+            var array = ['piedra', 'papel', 'tijera'];
+            var numero = Math.floor(Math.random()*3);
+            return array[numero];
+        }
+        //Comparacion
+        var resultado = comparacion(nombre,compu);
+        var mensaje = document.getElementById('mensaje').innerHTML=`Tú ${resultado} escogiendo ${nombre} contra de ${compu}`;
+        //console.log(resultado);
+
+        function comparacion(nombre, compu){
+            if(nombre == compu){
+                return "Empate";
+            }
+            if((nombre == 'papel' && compu == 'piedra') || (nombre == 'tijera' && compu == 'papel') || (nombre == 'piedra' && compu == 'tijera')){
+                return "ganas";
+            }else if((nombre == 'papel' && compu == 'tijera') || (nombre == 'tijera' && compu == 'piedra') || (nombre == 'piedra' && compu == 'papel')){
+                return "pierdes";
+            }
         }
     });
 });
